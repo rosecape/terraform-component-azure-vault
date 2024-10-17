ac1dd783de6ab1fb350cd5f573b161c3f06c0772
Nicholas Joanisse <nicholas@rosecape.ca>
2024-10-17 09:35:42 -0400

    outputs: add key vault name to output

--- main.tf
+++ main.tf
@@ -143,7 +143,6 @@ resource "azurerm_key_vault" "core" {
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
-  enable_rbac_authorization   = true
   sku_name                    = "standard"
 
   timeouts {
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "key_vault_uri" {
   value = azurerm_key_vault.core.vault_uri
 }
+
+output "key_vault_name" {
+  value = azurerm_key_vault.core.name
+}
